ee632cf264c76a431799af9b78d2a801406f3057
Dragorn421 <Dragorn421@users.noreply.github.com>
2021-12-30 14:26:14 +0100

    Load to gameplay right after boot

--- src/code/title_setup.c
+++ src/code/title_setup.c
@@ -3,8 +3,26 @@
 void TitleSetup_InitImpl(GameState* gameState) {
     osSyncPrintf("ゼルダ共通データ初期化\n"); // "Zelda common data initalization"
     SaveContext_Init();
+
+    /*
+    // vanilla
     gameState->running = false;
     SET_NEXT_GAMESTATE(gameState, Title_Init, TitleContext);
+    */
+
+    gameState->running = false;
+    gSaveContext.gameMode = 0;
+    gSaveContext.linkAge = 0; // adult (1 for child)
+    Sram_InitDebugSave();
+    gSaveContext.dayTime = 0x8000;
+    gSaveContext.entranceIndex = 0;
+    while (!(gEntranceTable[gSaveContext.entranceIndex].scene == SCENE_SPOT00 &&
+             gEntranceTable[gSaveContext.entranceIndex].spawn == 0)) {
+        gSaveContext.entranceIndex++;
+    }
+    gSaveContext.respawnFlag = 0;
+    gSaveContext.respawn[RESPAWN_MODE_DOWN].entranceIndex = -1;
+    SET_NEXT_GAMESTATE(gameState, Gameplay_Init, GlobalContext);
 }
 
 void TitleSetup_Destroy(GameState* gameState) {
