8439b34dfe0e791312b3972a1f40e129f3e5da0c
qwoodmansee <qwoodmansee@DESKTOP-Q1KJN8C.localdomain>
2022-03-19 13:57:02 -0400

    windows dev scripts

---
+++ windows-build-and-reload.sh
@@ -0,0 +1,4 @@
+cd ~/code/oot
+make -j
+TASKKILL.exe /IM Project64.exe
+/mnt/d/pj64/Project64.exe zelda_ocarina_mq_dbg.z64
---
+++ windows-reload.sh
@@ -0,0 +1,2 @@
+TASKKILL.exe /IM Project64.exe
+/mnt/d/pj64/Project64.exe zelda_ocarina_mq_dbg.z64
