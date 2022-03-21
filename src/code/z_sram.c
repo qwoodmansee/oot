1e9fe1e29b68dbdb9901a6f8465f58e6cc7a586e
qwoodmansee <q.woodmansee@yahoo.com>
2022-03-21 15:54:07 -0400

    hover boot specific dev

--- src/code/z_sram.c
+++ src/code/z_sram.c
@@ -215,7 +215,7 @@ static SavePlayerData sDebugSavePlayerData = {
 static ItemEquips sDebugSaveEquips = {
     { ITEM_SWORD_MASTER, ITEM_BOW, ITEM_BOMB, ITEM_OCARINA_FAIRY }, // buttonItems
     { SLOT_BOW, SLOT_BOMB, SLOT_OCARINA },                          // cButtonSlots
-    0x1122,                                                         // equipment
+    0x3132, //0x3132 for hovers                                                         // equipment
 };
 
 static Inventory sDebugSaveInventory = {
@@ -223,7 +223,7 @@ static Inventory sDebugSaveInventory = {
         ITEM_STICK,     ITEM_NUT,           ITEM_BOMB,         ITEM_BOW,         ITEM_ARROW_FIRE,  ITEM_DINS_FIRE,
         ITEM_SLINGSHOT, ITEM_OCARINA_FAIRY, ITEM_BOMBCHU,      ITEM_HOOKSHOT,    ITEM_ARROW_ICE,   ITEM_FARORES_WIND,
         ITEM_BOOMERANG, ITEM_LENS,          ITEM_BEAN,         ITEM_HAMMER,      ITEM_ARROW_LIGHT, ITEM_NAYRUS_LOVE,
-        ITEM_BOTTLE,    ITEM_POTION_RED,    ITEM_POTION_GREEN, ITEM_POTION_BLUE, ITEM_POCKET_EGG,  ITEM_WEIRD_EGG,
+        ITEM_BOTTLE,    ITEM_POTION_RED,    ITEM_POTION_GREEN, ITEM_POTION_BLUE, ITEM_BOMBCHU,  ITEM_BOMBCHU,
     },                                                              // items
     { 50, 50, 10, 30, 1, 1, 30, 1, 50, 1, 1, 1, 1, 1, 1, 1 },       // ammo
     0x7777,                                                         // equipment
